88989e38150ef796e4d8d7b1414307a2648848d1
anjalimedini <anjali.medini@jukshio.com>
2024-05-17 11:26:10 +0530

    add new code

--- cam.py
+++ cam.py
@@ -1,37 +1,24 @@
-import cv2 
-  
-video = cv2.VideoCapture(0) 
-    
-if (video.isOpened() == False):  
-    print("Error reading video file") 
-   
-frame_width = int(video.get(3)) 
-frame_height = int(video.get(4)) 
-   
-size = (frame_width, frame_height) 
-   
-result = cv2.VideoWriter('camera.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         10, size) 
-    
-while(True): 
-    ret, frame = video.read() 
-  
-    if ret == True:  
-  
-        result.write(frame) 
-        cv2.imshow('Frame', frame) 
-  
-        if cv2.waitKey(1) & 0xFF == ord('s'): 
-            break
-  
-    else: 
+import cv2
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--WIDTH",type=int,help="enter width value")
+parser.add_argument("--HEIGHT",type=int,help="enter height value")
+parser.add_argument("--SIZE: WIDTH,HEIGHT")
+args = parser.parse_args()
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+size = (width, height)
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('web_video1.avi', fourcc, 20.0, size)
+
+while(True):
+    _, frame = cap.read()
+    cv2.imshow('Recording...', frame)
+    out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-  
-video.release() 
-result.release() 
-    
-cv2.destroyAllWindows() 
-   
 
-
+cap.release()
+out.release()
+cv2.destroyAllWindows()
